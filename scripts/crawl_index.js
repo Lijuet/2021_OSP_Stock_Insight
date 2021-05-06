1ec882ce17f251e3e258193e851a8cf724c33461
SeleeChae <seleechae@gmail.com>
2021-05-06 15:34:04 +0900

    0506 add result comment for crawl index module

--- scripts/crawl_index.js
+++ scripts/crawl_index.js
@@ -2,27 +2,68 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const log = console.log;
 
-const naverFinaceKoreaURL = "https://finance.naver.com/sise/";
-const naverFinaceWorldURL = "https://finance.naver.com/world/";
-const indexKoreaCategory = ["KOSPI", "KOSDAQ", "KPI200"];
-
-const getHtml = async (url) => {
+async function getHtml(url) {
     try {
         return await axios.get(url);
     } catch (error) {
         console.error(error);
     }
-};
+}
 
-getHtml(naverFinaceKoreaURL)
-    .then((html) => {
-        let indexKorea = {};
-        const $ = cheerio.load(html.data);
+const naverFinaceKoreaURL = "https://finance.naver.com/sise/";
+const naverFinaceWorldRootURL =
+    "https://finance.naver.com/world/sise.nhn?symbol=";
+const indexKoreaCategory = ["KOSPI", "KOSDAQ", "KPI200"];
+const indexWorldCategory = ["DJI@DJI", "NAS@IXIC", "SPI@SPX"];
+let indexWorld = {};
+
+async function getKoreaIndex() {
+    let indexKorea = {};
+    try {
+        let html = await getHtml(naverFinaceKoreaURL);
+        const $ = await cheerio.load(html.data);
 
         indexKoreaCategory.forEach((index) => {
             indexKorea[index] = $("#" + index + "_now").text();
         });
-
+        log(indexKorea);
         return indexKorea;
-    })
-    .then((res) => log(res));
+    } catch {
+        return {
+            KOSPI: 0,
+            KOSDAQ: 0,
+            KPI200: 0,
+        };
+    }
+}
+
+async function getWorldIndex() {
+    await axios
+        .all(
+            indexWorldCategory.map((idx) => {
+                return getHtml(naverFinaceWorldRootURL + idx);
+            })
+        )
+        .then(
+            axios.spread((...responses) => {
+                responses.forEach((html) => {
+                    const $ = cheerio.load(html.data);
+                    let name = $("div.h_area em").text();
+                    let value = $("p.no_today em").children("span").text();
+                    indexWorld[name.substring(0, 3)] = value;
+                });
+                log(indexWorld);
+            })
+        );
+    return indexWorld;
+}
+log(getKoreaIndex());
+log(getWorldIndex());
+
+/*
+RESULT : 
+Promise { <pending> }
+Promise { <pending> }
+{ DJI: '34,230.34', NAS: '13,582.43', SPI: '4,167.59' }
+{ KOSPI: '3,171.13', KOSDAQ: '969.76', KPI200: '425.29' }
+*/
