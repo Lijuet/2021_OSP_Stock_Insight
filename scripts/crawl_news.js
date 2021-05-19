c1c9cbfde8e3f859d691a82f3c2ac0799e432849
SeleeChae <seleechae@gmail.com>
2021-05-19 11:15:08 +0900

    0519 crawl news related with stock code

--- scripts/crawl_news.js
+++ scripts/crawl_news.js
@@ -1,25 +1,40 @@
 const axios = require("axios");
-const cheerio = require("cheerio");
 
-async function getHtml(url) {
+async function getHtml(url, code) {
     try {
-        return await axios.get(url);
+        return await axios.get(url, {
+            headers: {
+                Referer: "https://finance.daum.net/quotes/A" + code,
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+            },
+        });
     } catch (error) {
-        console.log("Fail to get html");
+        console.log("FAIL TO LOAD HTML");
     }
 }
-async function getKoreaIndex(keyword) {
-    const searchURL = "";
-    let news = {
-        head: "",
-        link: "",
-    };
+async function getNews(code) {
+    const searchURL =
+        "https://finance.daum.net/content/news?perPage=5&category=economy&searchType=all&keyword=A" +
+        code;
     let newsList = [];
 
-    let html = await getHtml(searchURL + keyword);
-    if (html === undefined) return newsList;
+    let html = await getHtml(searchURL, code);
+    let news = html.data["data"];
 
-    const $ = cheerio.load(html.data);
+    news.filter((_news) => _news["title"] && _news["imageUrl"]).map((_news) => {
+        newsList.push({
+            head: _news["title"],
+            link: _news["imageUrl"],
+            summary: _news["summary"],
+        });
+    });
 
     return newsList;
 }
+
+module.exports = { getNews };
+
+getNews("000660").then((ret) => {
+    console.log(ret);
+});
