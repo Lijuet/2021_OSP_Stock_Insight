2fe417a7e4209bb3376cd62c68c306b1de1b9b27
SeleeChae <seleechae@gmail.com>
2021-05-23 13:39:04 +0900

    0523 fix exception case & add more examples

--- scripts/stock_code.js
+++ scripts/stock_code.js
@@ -1,31 +1,34 @@
 const fs = require("fs");
 
-function getCode2NameDict(fileName) {
+function getCode2NameDict() {
     const file_csv = fs.readFileSync("./data/stock_code_data.csv");
     const stock_code = file_csv.toString();
-    const rows = stock_code.split("\r\n");
+    const rows = stock_code.split("\n");
 
     const code2name = [];
     for (let i = 1; i < rows.length; i++) {
         row = rows[i].split(",");
         const code = "0".repeat(6 - row[0].length) + row[0];
 
-        code2name[code] = row[1];
+        if (code && row[1]) code2name[code] = row[1];
+        if (code === "005930") code2name[code] = "삼성전자";
     }
+    // console.log(code2name);
     return code2name;
 }
 
-function getName2CodeDict(fileName) {
+function getName2CodeDict() {
     const file_csv = fs.readFileSync("./data/stock_code_data.csv");
     const stock_code = file_csv.toString();
-    const rows = stock_code.split("\r\n");
+    const rows = stock_code.split("\n");
 
     const name2code = [];
     for (let i = 1; i < rows.length; i++) {
         row = rows[i].split(",");
         const code = "0".repeat(6 - row[0].length) + row[0];
 
-        name2code[row[1]] = code;
+        if (code && row[1]) name2code[row[1]] = code;
+        if (code === "005930") name2code["삼성전자"] = "005930";
     }
     return name2code;
 }
@@ -43,7 +46,9 @@ function getNameWithCode(stockCode) {
 }
 
 // 주식 이름 입력시 주식 코드값 얻는 방법
-console.log(getCodeWithName("포스코엠텍"));
+console.log(getCodeWithName("삼성전자"));
+console.log(getCodeWithName("한진칼우"));
 
 // 주식 코드값 입력시 주식 이름 얻는 방법
-console.log(getNameWithCode("009520"));
+console.log(getNameWithCode("005930"));
+console.log(getNameWithCode("18064K"));
